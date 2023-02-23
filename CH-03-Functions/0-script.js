0e889f4733ff0bf98acbd83341684538319ed4dc
Tifannie Truman <nietru143@gmail.com>
2023-02-23 15:56:44 -0500

    finished Ch-03 functions notes

--- CH-03-Functions/0-script.js
+++ CH-03-Functions/0-script.js
@@ -1,4 +1,4 @@
-// FUNCTIONS
+// ***** FUNCTIONS ******
 
 // A function is created with an expression that starts with the Keyword "function()".
 // Functions can have multiple parameters, or none at all.
@@ -139,3 +139,119 @@ console.log("Bye");
 // If the stack gets too large, the computer will fail with a message like: "out of stack space" or "too much recursion".
 //-----------------------------------------------------
 // OPTIONAL ARGUMENTS:
+function square(x) {
+  return x * x;
+}
+console.log(square(4, true, "hedgehog"));
+// ^^ that is allowed to execute, though we defined square with only one parameter. When we call it with 3, it ignores the extra arguments.
+// for example: the following version of 'power' makes its second argument optional:
+function powerr(base, exponent = 2) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+}
+// if you dont provide a second argument, or pass the value 'undefined', it will default to two, and behave like a square:
+console.log(powerr(4));
+// --> 16
+console.log(powerr(4, 2));
+// --> 16
+//--------------------------------------------------------
+// CLOSURE:
+// Think of function values as containing both the code in their body, and the environment in which they were created.
+// When called, the function body sees environment in which is was created, not the environment where it is called.
+function multiplier(factor) {
+  return (number) => number * factor;
+}
+let twice = multiplier(2);
+console.log(twice(5));
+// --> 10
+// ^^ multiplier is called and creates environment where 'factor' is bound to 2.
+//  the function value it returns is stored in 'twice' binding/variable, and when it is called, it multiplies its argument by 2.
+//---------------------------------------------------------------
+// RECURSION: when a function calls ITSELF = "recursive function"
+// recursion is fine, as long as it does not do so so often that it overflows the stack (computer memory).
+// Recursion allows some functions to be written in a different style: (commented out because throws error in console " max stack size exceeded")
+// function powerr(base, exponent) {
+//   if (exponent == 0) {
+//     return 1;
+//   } else {
+//     return base * powerr(base, exponent - 1);
+//   }
+// }
+// console.log(power(2, 3));
+// --- Consider this puzzle, and the recursive code solution:
+function findSolution(target) {
+  // the inner 'find' function does the actual recursing: it takes 2 arguments: current number and a string to record how we reached this number.
+  function find(current, history) {
+    // if it finds a solution, it returns a string that shows HOW to get to the taret.
+    if (current == target) {
+      return history;
+      // if no solution can be found from starting number, it returns null.
+    } else if (current > target) {
+      return null;
+    } else {
+      return (
+        find(current + 5, `(${history} + 5)`) ||
+        find(current * 3, `(${history} * 3)`)
+      );
+    }
+  }
+  return find(1, "1");
+}
+console.log(findSolution(24));
+// --> (((1 * 3) + 5) *3)
+//-----------------------------------------------------------
+// GROWING FUNCTIONS:
+// We want to write a program that prints two numbers: number of chickens and the number of cows on a farm,
+//  with the words: Cows and Chickens, and zeros padded before them so they are three digits long:
+function printFarmInventory(cows, chickens) {
+  let cowString = String(cows);
+  while (cowString.length < 3) {
+    // .length gives us the length of this string. The while loop will keep adding zeros to the front until there are 3 digits.
+    cowString = "0" + cowString;
+  }
+  console.log(`${cowString} Cows`);
+  let chickenString = String(chickens);
+  while (chickenString.length < 3) {
+    chickenString = "0" + chickenString;
+  }
+  console.log(`${chickenString} Chickens`);
+}
+printFarmInventory(7, 11); // calling the function^^
+// The farmer calls us ^^ and wants to add pigs, lets do this whole program a better way: by picking out a single concept
+function zeroPad(number, width) {
+  let string = String(number);
+  while (string.length < width) {
+    string = "0" + string;
+  }
+  return string;
+}
+function printFarmInventory(cows, chickens, pigs) {
+  console.log(`${zeroPad(cows, 3)} Cows`);
+  console.log(`${zeroPad(chickens, 3)} Chickens`);
+  console.log(`${zeroPad(pigs, 3)} Pigs`);
+}
+printFarmInventory(7, 16, 3);
+// ^^ A function with a nice obvious name like 'zeroPad' makes it more readable for other devs.
+// TIP: A useful principal is to NOT add cleverness unless you are absolutely going to need it.
+// It can be tempting to write general "frameworks" for every bit of code, but you will just be writing code that you'll never use!!
+// --------------------------------------------------------------
+// CHAPTER SUMMARY:
+// The function keyword can create a function value, when used as an expression.
+// When used as a statement, it can be used to declare a binding and give it a function as its' value.
+// Arrow functions are yet another way to create functions.
+//---
+// Define f to hold a function value:
+const f = function (a) {
+  console.log(a + 2);
+};
+// Declare g to be a function:
+function g(a, b) {
+  return a * b * 3.5;
+}
+// A less verbose function value:
+let h = (a) => a % 3;
+
+// Separating your program into different functions for different things is very helpful!
